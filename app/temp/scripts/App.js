2c32ed9cdb81c6e57b3775f4bc6c31d13fd0bdb4
Ed <gardame@gmail.com>
2017-12-10 12:01:21 +1100

    added hamburger menu animations

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -103,20 +103,23 @@ var MobileMenu = function () {
   function MobileMenu() {
     _classCallCheck(this, MobileMenu);
 
+    this.siteHeader = (0, _jquery2.default)('.site-header');
     this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
     this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
     this.events();
   }
 
   _createClass(MobileMenu, [{
-    key: "events",
+    key: 'events',
     value: function events() {
       this.menuIcon.click(this.toggleTheMenu.bind(this));
     }
   }, {
-    key: "toggleTheMenu",
+    key: 'toggleTheMenu',
     value: function toggleTheMenu() {
       this.menuContent.toggleClass("site-header__menu-content--is-visible");
+      this.siteHeader.toggleClass("site-header--is-expanded");
+      this.menuIcon.toggleClass("site-header__menu-icon--close-x");
     }
   }]);
 
